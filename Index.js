bed9b55871c8b1bf680e32158f35e53e1251f8b0
Cornmos <72299677+Cornmos@users.noreply.github.com>
2021-10-03 11:27:04 +0930

    Server side Code Implementation
    A server side Javascript file has been created that handles server requests and save data to the database. All files inside the public folder are visible to the client

---
+++ Index.js
@@ -0,0 +1,12 @@
+//Server code
+const express = require("express");
+fs = require('fs');
+const app= express();
+app.listen(3000, () => console.log("listening on port 3000"));
+app.use(express.static('public'));
+app.use(express.json());
+app.post('/api',(request,response)=>{console.log(request.body);
+console.log("working");
+fs.appendFileSync('database.txt',JSON.stringify(request.body)+",\n");
+response.json(request.body)
+});
